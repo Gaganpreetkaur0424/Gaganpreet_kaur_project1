37f97f76e2598120cfdb3b53a89a90556a958be9
Gaganpreetkaur0424 <73954866+Gaganpreetkaur0424@users.noreply.github.com>
2021-10-31 00:25:33 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require('mysqli_connect.php');
+session_start();
+$query = "SELECT * FROM bookinventory";
+$result = @mysqli_query($dbc, $query);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Book Store</title>
+</head>
+<span><a href=index.php>home </a></span>
+<span><a href=store.php>book store </a></span>
+<span><a href=checkout.php>checkout </a></span>
+<body>
+    <main>
+        <div class="row">
+           
+        </div>
+    </main>
+</body>
+</html>
